edf6730795b4cfa45600e17a13796e6e2661ddd1
Ronnyyx <apazaronny4@gmail.com>
2022-12-06 00:45:52 -0500

    Agente Versión 1.4 recursos

---
+++ recursos/Libreria.php
@@ -0,0 +1,165 @@
+<?php
+session_start();
+
+abstract class Libreria {
+    static function getMenu($tipo=1){  # Generamos el MENU de opciones
+        $menuAdmin= array(
+            array(
+                'icono'=>'user',
+                'enlace'=>'ctrl=CtrlUsuario1',
+                'texto'=>'USUARIOS'
+                
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlAgente',
+                'texto'=>'Agente'
+
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlBancos',
+                'texto'=>'Bancos'
+            
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlcuentasUsuario',
+                'texto'=>'Cuentas de Usuario'
+                
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlEmpresa',
+                'texto'=>'Empresa'
+
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlTipoTransaccion',
+                'texto'=>'Tipo de Transaccion'
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlTipoUsuario',
+                'texto'=>'Tipo de Usuario'
+            
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlVoucher',
+                'texto'=>'Voucher'
+            )
+        );
+        $menuCliente= array(
+            
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlUsuario&accion=verSaldo',
+                'texto'=>'Ver saldo'
+                
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlCliente&accion=cambarContraseña',
+                'texto'=>'Consulta de Movimientos'
+            ),
+            array(
+                'icono'=>'table',
+                'enlace'=>'ctrl=CtrlCliente&accion=cambarContraseña',
+                'texto'=>'Cambiar Contraseña'
+            )
+        );
+
+        switch ($tipo) {
+            case 1:
+                $menu = $menuAdmin;
+                break;
+            case 2:
+                $menu = $menuCliente;
+                break;
+            
+            default:
+               $menu=null;
+                break;
+        }
+        return $menu;
+    }
+    static function cssGlobales(){
+        return array(
+                array(
+                    'nombre'=>'Google Font: Source Sans Pro',
+                    'url'=>'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback'
+                ),
+                
+                array(
+                    'nombre'=>'Font Awesome',
+                    'url'=>'plugins/fontawesome-free/css/all.min.css'
+                ),
+                array(
+                    'nombre'=>'Ionicons',
+                    'url'=>'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'
+                ),
+                array(
+                    'nombre'=>'Tempusdominus Bootstrap 4',
+                    'url'=>'recursos/css/bootstrap.min.css'
+                    # 'url'=>'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'
+                ),
+                array(
+                    'nombre'=>'iCheck',
+                    'url'=>'plugins/icheck-bootstrap/icheck-bootstrap.min.css'
+                ),
+                array(
+                    'nombre'=>'JQVMap',
+                    'url'=>'plugins/jqvmap/jqvmap.min.css'
+                ),
+                array(
+                    'nombre'=>'Theme style',
+                    'url'=>'dist/css/adminlte.min.css'
+                ),
+                array(
+                    'nombre'=>'overlayScrollbars',
+                    'url'=>'plugins/overlayScrollbars/css/OverlayScrollbars.min.css'
+                ),
+                array(
+                    'nombre'=>'Daterange picker',
+                    'url'=>'plugins/daterangepicker/daterangepicker.css'
+                ),
+                array(
+                    'nombre'=>'summernote',
+                    'url'=>'plugins/summernote/summernote-bs4.min.css'
+                ),
+                array(
+                    'nombre'=>'jsToast',
+                    'url'=>'recursos/css/jquery/jquery-toast.css'
+                )
+            );
+    }
+    
+    static function jsGlobales(){
+        return array(
+            array(
+                'url'=>'plugins/jquery/jquery.min.js'
+            ),
+            array(
+                'url'=>'plugins/jquery-ui/jquery-ui.min.js'
+            ),
+            array(
+                'url'=>'plugins/bootstrap/js/bootstrap.bundle.min.js'
+            ),
+            array(
+                'url'=>'dist/js/adminlte.js'
+            ),
+            array(
+                'url'=>'dist/js/demo.js'
+            ),
+            array(
+                'url'=>'dist/js/pages/dashboard3.js'
+            ),
+            array(
+                'url'=>'recursos/js/jq-toast.min.js'
+            ),
+        );
+    }
+
+}
